7f36bb6ed0c2542c8b4dd21f34aac53c22fb4099
yuheiTy <xiongpingdongban@gmail.com>
2022-04-23 16:29:08 +0900

    jstest

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const next = document.getElementById("next");
 const ul = document.querySelector("ul");
+const slides = ul.children;
 next.addEventListener('click',()=> {
-    ul.style.transform = "translateX(-200px)";
+    const slidewidth = slides[0].getBoundingClientRect().width;
+    ul.style.transform = `translateX(${-1*slidewidth}px)`;
 })

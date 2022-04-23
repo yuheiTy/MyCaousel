2589c3b21691c76ad0cd62da469a2d5a1477c6f3
yuheiTy <xiongpingdongban@gmail.com>
2022-04-23 16:52:02 +0900

    prev

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const next = document.getElementById("next");
+const prev = document.getElementById("prev");
 const ul = document.querySelector("ul");
 const slides = ul.children;
+let currentIndex = 0;
+
 next.addEventListener('click',()=> {
+    currentIndex++;
     const slidewidth = slides[0].getBoundingClientRect().width;
-    ul.style.transform = `translateX(${-1*slidewidth}px)`;
+    ul.style.transform = `translateX(${-1*slidewidth * currentIndex}px)`;
+})
+
+prev.addEventListener('click',()=> {
+    currentIndex--;
+    const slidewidth = slides[0].getBoundingClientRect().width;
+    ul.style.transform = `translateX(${-1*slidewidth * currentIndex}px)`;
 })
